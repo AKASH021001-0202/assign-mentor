8bc6bb51510b47469ef9f954e4f0fa2204b52908
AKASH021001-0202 <nakashn007@gmail.com>
2024-06-10 16:18:28 +0530

    final

--- db.utils/mongoose-connection.js
+++ db.utils/mongoose-connection.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 const dbName ="nodedb";
+
+const  dnUserName = "nakashn007";
+
+const  dbPassword = "fYnVb2BelJcAggC0";
 const dbCluster = "127.0.0.1:27017";
 
-const localUri =`mongodb://${dbCluster}/${dbName}`;
+/* // const localUri =`mongodb://${dbCluster}/${dbName}`; */
+const cloudUri =`mongodb+srv://${dnUserName}:${dbPassword}@cluster0.ppqlls3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 const mongooseDb = async () => {
     try{
-        await mongoose.connect(localUri);
+        await mongoose.connect(cloudUri);
         console.log("mongodb:connect success")
     }
     catch(err){
